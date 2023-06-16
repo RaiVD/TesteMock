436c0ef11e23c11c3654de236349932dd940fc52
Ana.Vitoria.Nunes <ana.nunes@zup.com.br>
2023-06-15 22:01:05 -0300

    Atualização

--- MelhoresDias/Gerentes.java
+++ MelhoresDias/Gerentes.java
@@ -1,2 +1,45 @@
 public class Gerentes {
+    private int id;
+    private String nome;
+    private String cpf;
+
+    public Gerentes(int id, String nome, String cpf) {
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    public  void VerificarSeGerenteExiste(String nome,String cpf,int id){
+        if(nome==getNome() && cpf==getCpf() && id==getId()){
+            System.out.println("Esse gerente está cadastrado no sistema");
+        }
+        else {
+            System.out.println("Não temos um gerente com todas essas informações!");
+        }
+
+    }
 }
+
--- MelhoresDias/GerentesTeste.java
+++ MelhoresDias/GerentesTeste.java
@@ -1,2 +1,12 @@
+import org.junit.jupiter.api.Test;
 public class GerentesTeste {
+
+    @Test
+    public void verificarSeGerenteEstaCadastrado(){
+        Gerentes gerentes=new Gerentes(123,"Ana","157.858.146-09");
+
+
+
+    }
+
 }
