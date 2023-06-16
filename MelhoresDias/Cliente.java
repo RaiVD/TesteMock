ebc3541da407edaa1c32b9bbf7c301203b34d9e2
Raissa Vicente <raissa.carvalho@zup.com.br>
2023-06-16 09:41:12 -0300

    Atualização

--- MelhoresDias/Cliente.java
+++ MelhoresDias/Cliente.java
@@ -1,23 +1,4 @@
-import java.time.DayOfWeek;
-import java.time.LocalDate;
-import java.time.format.TextStyle;
-import java.util.Locale;
 
 public class Cliente {
-    public static String diaDaSemanaAtual(){
-        DayOfWeek diaAtualDaSemana = LocalDate.now().getDayOfWeek();
-        String nomeDiaSemana = diaAtualDaSemana.getDisplayName(TextStyle.FULL, Locale.getDefault());
 
-        return "Hoje é "+nomeDiaSemana;
-    }
-    public static String melhorDiaFeira() {
-        DayOfWeek dataAtual = LocalDate.now().getDayOfWeek();
-
-        if (dataAtual == DayOfWeek.SUNDAY) {
-            return "Hoje é domingo, o melhor dia para ir à feira.";
-        } else {
-            int diasQueFaltam = DayOfWeek.SUNDAY.getValue() - dataAtual.getValue();
-            return "Ainda não é um bom dia para ir a feira, aguarde " + diasQueFaltam + " dias para domingo.";
-        }
-    }
 }
--- MelhoresDias/Vendedores.java
+++ MelhoresDias/Vendedores.java
@@ -1,2 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Vendedores {
+    static Map<Integer, Vendedores> listaDeVendedoresAtivos = new HashMap();
+    public String nome;
+    public String cpf;
+    public Vendedores(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+    public static void verificarSeExistiUmVendedor(int valor) {
+        listaDeVendedoresAtivos.put(123, new Vendedores("Raissa", "12345678903"));
+        listaDeVendedoresAtivos.put(122, new Vendedores("Ana", "12345678904"));
+        listaDeVendedoresAtivos.put(133, new Vendedores("Renato", "12345678905"));
+
+        if (listaDeVendedoresAtivos.containsKey(valor)){
+            System.out.println("Vendedor cadastrado!");
+        }else{
+            System.out.println("Vendedor não cadastrado");
+        }
+    }
 }
